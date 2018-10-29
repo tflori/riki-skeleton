20f0fb6e4762f74220799a51204c61f9794b1ff5
Thomas Flori <thflori@gmail.com>
2018-10-29 08:27:33 +0100

    add code sniffer and code style for dev environment

--- skeleton/dev/composer.json.tpl.php
+++ skeleton/dev/composer.json.tpl.php
@@ -8,5 +8,11 @@
 $content->mergeJson([
     'require' => [
         'tflori/riki-framework' => 'dev-master',
-    ]
+    ],
+    'require-dev' => [
+        'squizlabs/php_codesniffer' => '^3.3',
+    ],
+    'scripts' => [
+        'code-style' => 'phpcs --standard=PSR2 app && phpcs --standard=PSR2 --ignore=example tests',
+    ],
 ]);
